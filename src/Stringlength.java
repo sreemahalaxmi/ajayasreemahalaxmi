f4ca4c1416a9a91d31fa074ec3fbc6a8f9cd2f5c
sreemahalaxmi <105187140+sreemahalaxmi@users.noreply.github.com>
2022-05-25 09:49:14 +0530

    Commit uploaded

---
+++ src/Stringlength.java
@@ -0,0 +1,9 @@
+
+public class Stringlength {
+    public static void main(String args[]) {
+	    String str="Hello World";
+	    int length=str.length();
+    	System.out.println("string length is:"+length);
+    }
+
+}
